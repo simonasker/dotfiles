0787543cdf04336c670f57905a3f5aa6aa985b86
Simon Andersson <simon.asker@gmail.com>
2015-06-18 10:45:12 +0200

    Vim: Add vim-fugitive

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -1,3 +1,4 @@
 git clone git://github.com/klen/python-mode.git
 git clone git://github.com/tpope/vim-commentary.git
 git clone git://github.com/tpope/vim-surround.git
+git clone git://github.com/tpope/vim-fugitive.git
