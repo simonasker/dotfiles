35a65da3bd2ad33c2e7772988f2dd85f9ed497d3
Simon Andersson <simon.asker@gmail.com>
2015-06-18 10:40:20 +0200

    Vim: Remove NERDTree

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/scrooloose/nerdtree.git
 git clone git://github.com/klen/python-mode.git
 git clone git://github.com/tpope/vim-commentary.git
 git clone git://github.com/tpope/vim-surround.git
