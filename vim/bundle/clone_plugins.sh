cb613a2aa4e856a506607248f0a3740bdf9745c0
Simon Andersson <simon.asker@gmail.com>
2015-10-25 10:04:51 +0100

    Vim: Add the vim-airline plugin

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -3,3 +3,4 @@ git clone git://github.com/tpope/vim-commentary.git
 git clone git://github.com/tpope/vim-surround.git
 git clone git://github.com/tpope/vim-fugitive.git
 git clone git@github.com:tpope/vim-unimpaired.git
+git clone git@github.com:bling/vim-airline.git
