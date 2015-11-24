482a64ca45ea27b2b25631ef8d99db8ee285c26d
Simon Andersson <simon.asker@gmail.com>
2015-11-24 21:25:40 +0100

    Vim: Add my own new vim-todo plugin

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -4,3 +4,4 @@ git clone git://github.com/tpope/vim-surround.git
 git clone git://github.com/tpope/vim-fugitive.git
 git clone git@github.com:tpope/vim-unimpaired.git
 git clone git@github.com:bling/vim-airline.git
+git clone git@github.com:simonasker/vim-todo.git
