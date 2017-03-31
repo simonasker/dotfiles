e94230aa2a29d59b8c0205501caef98f3de38ac9
Simon Andersson <simon.asker@posteo.net>
2017-03-31 21:44:43 +0200

    Remove some vim plugins

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -1,8 +1,3 @@
 git clone git://github.com/klen/python-mode.git
 git clone git://github.com/tpope/vim-commentary.git
-git clone git://github.com/tpope/vim-surround.git
-git clone git://github.com/tpope/vim-fugitive.git
-git clone git@github.com:tpope/vim-unimpaired.git
-git clone git@github.com:bling/vim-airline.git
-git clone git@github.com:simonasker/vim-todo.git
 git clone git@github.com:rust-lang/rust.vim.git
