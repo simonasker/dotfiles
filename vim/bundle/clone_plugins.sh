2fd95a23699fe5d04ba8c913c29b9d8762ca4c4c
Simon Andersson <simon@mullvad.net>
2017-05-25 09:03:09 +0200

    Add and configure vim airline

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -2,3 +2,6 @@ git clone git://github.com/klen/python-mode.git
 git clone git://github.com/tpope/vim-commentary.git
 git clone git@github.com:rust-lang/rust.vim.git
 git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
