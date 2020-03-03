22b0102ecf81b05db84c03a493ff0f911204e7de
Simon Andersson <simon@mullvad.net>
2020-03-03 15:18:14 +0100

    Clean up vim config

--- vim/bundle/clone_plugins.sh
+++ vim/bundle/clone_plugins.sh
@@ -1,10 +1,11 @@
 git clone git://github.com/tpope/vim-commentary.git
 git clone git@github.com:rust-lang/rust.vim.git
 git clone git://github.com/tpope/vim-fugitive.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone git@github.com:vim-syntastic/syntastic.git
 
 # Previously used plugins
+# git clone https://github.com/scrooloose/nerdtree.git
+# git clone git@github.com:vim-syntastic/syntastic.git
+# git clone git@github.com:prettier/vim-prettier.git
 # git clone https://github.com/vim-airline/vim-airline.git
 # git clone https://github.com/vim-airline/vim-airline-themes.git
 # git clone git://github.com/chase/vim-ansible-yaml.git
